10ed4515b61abb32ddfbc859fa716bcd0dc1a220
Tyler <nguyentyler97@gmail.com>
2022-09-23 19:11:26 -0700

    feat: add css and html for pop-up form

--- scripts.js
+++ scripts.js
@@ -2,21 +2,21 @@ let myLibrary = ['Harry Potter', 'Goosebumps', 'Percey Jackson'];
 
 function Book() {
     // the constructor...
-}
+};
 
 function addBookToLibrary() {
     // do stuff here
-}
+};
 
-const library = document.querySelector('.library')
+const library = document.querySelector('.library');
 
 function listBooks () {
     myLibrary.forEach(item => {
-        const div = document.createElement('div')
-        div.classList.add('book')
-        div.textContent = item
-        library.appendChild(div)
+        const div = document.createElement('div');
+        div.classList.add('book');
+        div.textContent = item;
+        library.appendChild(div);
     })
-}
+};
 
-listBooks()
+listBooks();
